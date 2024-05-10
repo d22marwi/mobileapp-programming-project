a9feaee3e257efce6b9944f350a4379518777d49
d22marwi <d22marwi@student.his.se>
2024-05-10 12:39:23 +0200

    Lade till gson, json i MainActivity.java.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=d22marwi";
+    private final String JSON_FILE = "Planets.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        new JsonTask(this).execute(JSON_URL);
+
     }
 
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Planet>>() {
+        }.getType();
+        List<Planet> ListOfPlanets = gson.fromJson(json, type);
+
+        for (Planet planet : ListOfPlanets) {
+            Log.d("Planet", planet.toString());
+        }
+    }
 }
