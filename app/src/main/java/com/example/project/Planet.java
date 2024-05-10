6d322a6054ac576d3265064047c69bbb574581e7
d22marwi <d22marwi@student.his.se>
2024-05-10 12:16:04 +0200

    lade till klassen Planet.java.

---
+++ app/src/main/java/com/example/project/Planet.java
@@ -0,0 +1,23 @@
+package com.example.project;
+
+public class Planet {
+
+    private String name;
+
+    public Planet(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}
